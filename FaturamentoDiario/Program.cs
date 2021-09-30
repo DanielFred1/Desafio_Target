3d8e05432e2e80158189fc2d150cc296ede73c15
DanielFred1 <dsena.frederic@gmail.com>
2021-09-30 12:05:05 -0300

    Faturamento Diário

--- FaturamentoDiario/Program.cs
+++ FaturamentoDiario/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Xml;
+using System.IO;
+using System.Collections.Generic;
 
 namespace FaturamentoDiario
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            XmlTextReader Leitor = new XmlTextReader("..\\FaturamentoDiario\\dados (2).xml");
+            while (Leitor.Read())
+            {
+                if (Leitor.NodeType == XmlNodeType.Text)
+                {
+                    Console.WriteLine(Leitor.Value);
+                }    
+            }
+
         }
     }
 }
