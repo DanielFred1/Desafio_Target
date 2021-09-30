7f48e3cc01daf46278683abd39de070992eb8727
DanielFred1 <dsena.frederic@gmail.com>
2021-09-30 13:35:08 -0300

    Finalização do algoritmo

--- FaturamentoMensal/Program.cs
+++ FaturamentoMensal/Program.cs
@@ -37,27 +37,33 @@ namespace FaturamentoMensal
             ListaEstados.Add(E4);
             ListaEstados.Add(Outros);
 
+            Console.Clear();
             double somatorio = 0;
 
+            Console.WriteLine("-------------------------------- Lista de Estados ---------------------------------");
             foreach (var item in ListaEstados)
             {
-                Console.Write(ListaEstados.IndexOf(item));
-                Console.WriteLine($" Estado: {item.Sigla}  Faturamento: R${item.Faturamento}");
+                Console.Write($"Index: {ListaEstados.IndexOf(item)}");
+                Console.WriteLine($" Estado: {item.Sigla}  Faturamento: {item.Faturamento.ToString("C")}");
 
                 somatorio += item.Faturamento;
             }
-
+            Console.WriteLine("-----------------------------------------------------------------------------------");
+            Console.WriteLine();
             double percentualE1 = (E1.Faturamento / somatorio)*100;
             double percentualE2 = (E2.Faturamento / somatorio)*100;
             double percentualE3 = (E3.Faturamento / somatorio)*100;
             double percentualE4 = (E4.Faturamento / somatorio)*100;
             double percentualOutros = (Outros.Faturamento / somatorio)*100;
-            Console.WriteLine($"O valor total de faturamento dos estados é de: R${somatorio}");
-            Console.WriteLine($"O percentual de representação do estado de {E1.Sigla} no valor total de faturamento é: {percentualE1}%");
-            Console.WriteLine($"O percentual de representação do estado de {E2.Sigla} no valor total de faturamento é: {percentualE2}%");
-            Console.WriteLine($"O percentual de representação do estado de {E3.Sigla} no valor total de faturamento é: {percentualE3}%");
-            Console.WriteLine($"O percentual de representação do estado de {E4.Sigla} no valor total de faturamento é: {percentualE4}%");
-            Console.WriteLine($"O percentual de representação do estado de {Outros.Sigla} no valor total de faturamento é: {percentualOutros}%");
+            Console.WriteLine($"O valor total de faturamento dos estados é de: {somatorio.ToString("C")}");
+            Console.WriteLine();
+            Console.WriteLine($"O percentual de representação do estado de {E1.Sigla} no valor total de faturamento é: {Math.Round(percentualE1, 2)}%");
+            Console.WriteLine($"O percentual de representação do estado de {E2.Sigla} no valor total de faturamento é: {Math.Round(percentualE2, 2)}%");
+            Console.WriteLine($"O percentual de representação do estado de {E3.Sigla} no valor total de faturamento é: {Math.Round(percentualE3, 2)}%");
+            Console.WriteLine($"O percentual de representação do estado de {E4.Sigla} no valor total de faturamento é: {Math.Round(percentualE4, 2)}%");
+            Console.WriteLine($"O percentual de representação de {Outros.Sigla} estados no valor total de faturamento é: {Math.Round(percentualOutros, 2)}%");
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------ Fim do Algoritmo ------------------------------------");
         }
     }
 }
