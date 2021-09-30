9435c99f5e155f1334d0b051eaa7c4f005ab2dc9
DanielFred1 <dsena.frederic@gmail.com>
2021-09-30 15:58:49 -0300

    Listagem json

--- FaturamentoDiario/Program.cs
+++ FaturamentoDiario/Program.cs
@@ -5,18 +5,37 @@ using System.Collections.Generic;
 
 namespace FaturamentoDiario
 {
+    public class Diario
+    {
+        public int Dia { get; set; }
+        public double Faturamento { get; set; }
+
+        public Diario(int dia, double faturamento)
+        {
+            Dia = dia;
+            Faturamento = faturamento;
+        }
+
+        public Diario()
+        {
+
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            var mes = new List<Diario>();
             XmlTextReader Leitor = new XmlTextReader("..\\FaturamentoDiario\\dados (2).xml");
             while (Leitor.Read())
             {
                 if (Leitor.NodeType == XmlNodeType.Text)
                 {
                     Console.WriteLine(Leitor.Value);
-                }    
+                }
+
+
             }
 
         }
